c8643e56c41ee034e2cd7a74b4190ea2a9b4860e
daudugodswill97 <daudugodswill2019@gmail.com>
2023-03-08 15:27:04 +0000

    working on the deposit form and logic

--- colbank/accounts/views.py
+++ colbank/accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views.generic import CreateView
 from . models import Deposit
 from .forms import DepositForm
@@ -8,6 +9,19 @@ from .forms import DepositForm
 
 def home(request):
     return render(request, 'home_list.html', {})
+
+def login_view(request):
+    if request.method == 'POST':
+         username = request.POST['username']
+         password = request.POST['password']
+         user = authenticate(request, username=username, password=password)
+         if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in!')
+            return redirect('dash_board')
+         else:
+            messages.error(request, 'Invalid username or password') 
+    return render(request, 'login.html')
 
 def dash_board(request):
     return render(request, 'dashboard.html',{})
