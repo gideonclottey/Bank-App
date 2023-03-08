6d3bc582d86fd4b2cc943852455346ce2d1e5ed7
Sonia-devi <kkashyapgirl@gmail.com>
2023-03-08 22:00:15 +0000

    wrong

--- colbank/colbank/urls.py
+++ colbank/colbank/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/about', views.about, name='about.list'.site.urls),
 ]
